f5270accc06b4213b55a2cbac54f3b0fbc63bc8f
Mohammad <b421027@iiit-bh.ac.in>
2023-10-23 01:15:03 +0530

    almost done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,9 +18,51 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false)
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    //d1-kzc1oW8-x-IMAU
+     //service_510zzy5
+     //template_13f4kvs
+
+    emailjs.send(
+      'service_510zzy5', 
+      'template_13f4kvs',
+      {
+        from_name: form.name,
+        to_name: 'Talha',
+        from_email: form.email,
+        to_email: 'b421027@iiit-bh.ac.in',
+        message: form.message,
+      },
+      'd1-kzc1oW8-x-IMAU'
+      )
+      .then(() => {
+        setLoading(false)
+        alert('Thank you for your message. I will get back to you soon!')
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        })
+      }, (error) => {
+        setLoading(false)
+        
+        console.log(error)
+        alert('Oops! Something went wrong. Please try again.')
+      })
+        
+  } 
+       
+
+      
+  
 
 
   return (
@@ -30,6 +72,7 @@ const Contact = () => {
           className="flex-[0.75] bg-black-100 p-8 rounded-2xl shadow-xl">
           <p className={styles.sectionSubText}>Get in Touch</p>
           <h3 className={styles.sectionHeadText}>Contact</h3>
+          
 
           <form
             ref={formRef}
