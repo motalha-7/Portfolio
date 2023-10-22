c31e9a7e564c345d7fc038929046d4e75db97806
Mohammad <b421027@iiit-bh.ac.in>
2023-10-22 14:44:42 +0530

    almost done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useState, useRef } from "react"
+import { motion } from "framer-motion"
+import emailjs from "@emailjs/browser"
+
+import { styles } from "../styles"
+import { SectionWrapper } from "../hoc"
+import { EarthCanvas } from "./canvas"
+import { slideIn } from "../utils/motion"
+
 
 const Contact = () => {
+  const formRef = useRef()
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = (e) => {}
+
+  const handleSubmit = (e) => {}
+
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+       <motion.div
+         variants={slideIn('left', "tween", 0.2, 1)}
+          className="flex-[0.75] bg-black-100 p-8 rounded-2xl shadow-xl">
+          <p className={styles.sectionSubText}>Get in Touch</p>
+          <h3 className={styles.sectionHeadText}>Contact</h3>
+
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-8 mt-12">
+              <label className="flex flex-col">
+                <span className="text-sm font-medium">Your Name</span>
+                <input
+                  type="text"
+                  name="name"
+                  value={form.name}
+                  onChange={handleChange}
+                  placeholder="Whats your name?"
+                  className="bg-tertiary border-none outlined-none rounded-lg py-4 px-6 text-white mt-2 placeholder:text-secondary font-medium"
+                />
+              </label>
+
+              <label className="flex flex-col">
+                <span className="text-sm font-medium">Your Email</span>
+                <input
+                  type="email"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  placeholder="Whats your email?"
+                  className="bg-tertiary border-none outlined-none rounded-lg py-4 px-6 text-white mt-2 placeholder:text-secondary font-medium"
+                />
+              </label>
+
+              <label className="flex flex-col">
+                <span className="text-sm font-medium">Your Message</span>
+                <textarea rows="7"
+                  name="message"
+                  value={form.message}
+                  onChange={handleChange}
+                  placeholder="What do you want to say?"
+                  className="bg-tertiary border-none outlined-none rounded-lg py-4 px-6 text-white mt-2 placeholder:text-secondary font-medium"
+                />
+              </label>
+
+              <button 
+                  type='submit'
+                  className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'>
+                   {loading ? "Sending..." : "Send" }
+              </button>
+          </form>
+        </motion.div>
+
+        <motion.div
+           variants={slideIn('right', "tween", 0.2, 1)}
+           className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+          <EarthCanvas />
+        </motion.div>
+      
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact")
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,9 +18,33 @@ const ProjectCard = ({ name, description, tags, source_code_link, image, index }
         }}
         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
 
-        <div className="flex justify-between items-center">
+        <div className="relative w-full h-[230px]">
+          <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl" />
+          
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div 
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+              <img src={github} alt="github" className="w-5 h-5 object-contain" />
+            </div>
+          </div>
+        </div>
 
+        <div className="mt-5">
+          <h3 className="text-white text-[20px] font-bold">
+            {name}
+          </h3>
+          <p className="mt-2 text-secondary text-[15px]">
+            {description}
+          </p>
+        </div>
 
+        <div className="mt-5 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
         </div>
       </Tilt>
     </motion.div>
@@ -57,13 +81,8 @@ const Works = () => {
             />
            ))}
         </div>
- 
-        
-
-
-        
       </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works , " ")
